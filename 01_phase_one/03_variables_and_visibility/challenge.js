ae459bd01d217d075ac9a351df71341474c5daa6
williamjones <willj836@gmail.com>
2022-12-12 11:01:32 +0000

    variables and visibility done

---
+++ 01_phase_one/03_variables_and_visibility/challenge.js
@@ -0,0 +1,14 @@
+// Can you find the problem in the following program?
+// Make the fix, so the code prints "Hello Makers" when run into node.
+
+// const greeting = 'Hello ';
+// const name = 'Makers';
+
+// console.log greeting + name;
+
+// Fixed code: 
+
+const greeting = "Hello ";
+const name = "Makers";
+
+console.log(greeting + name);
---
+++ 01_phase_one/03_variables_and_visibility/exercise.js
@@ -0,0 +1,10 @@
+// declare two variables — a with value 4 and b with value 6.
+// Using console.log, print the result of:
+// a + b (should print 10)
+// a * b (should print 24)
+
+const a = 4;
+const b = 6;
+
+console.log(a + b);
+console.log(a * b);
