3652ad3786cb9f0f34f148d990541338f94060e8
williamjones <willj836@gmail.com>
2022-12-14 16:52:25 +0000

    user interface done

--- 02_phase_two/06_weather_api/weather.js
+++ 02_phase_two/06_weather_api/weather.js
@@ -1,3 +1,10 @@
+const WeatherClient = require("./weatherClient.js");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 class Weather {
   constructor(client) {
     this.client = client;
@@ -16,6 +23,32 @@ class Weather {
   getWeatherData() {
     return this.data;
   }
-}
 
+  displayWeather() {
+    console.log(`City: ${this.data.name}`);
+    console.log(`Weather: ${this.data.weather[0].main}`);
+    console.log(`Temperature: ${Math.round(this.data.main.temp)}\u00B0C`);
+    console.log(`Feels like: ${Math.round(this.data.main.feels_like)}\u00B0C`);
+    console.log(`Humidity: ${this.data.main.humidity}%`);
+  }
+
+  userInput() {
+    rl.question(
+      "\nWhat city would you like to see the weather for? Type done when you would like to exit the program\n",
+      (input) => {
+        this.fetch(input).then((weather) => {
+          if (input === "done") {
+            rl.close();
+          } else {
+            this.displayWeather();
+            this.userInput();
+          }
+        });
+      }
+    );
+  }
+}
 module.exports = Weather;
+
+const weather = new Weather(new WeatherClient());
+weather.userInput();
