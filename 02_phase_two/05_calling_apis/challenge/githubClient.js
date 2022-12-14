2c99f821eaf6f8c87882ad1dabad2e8cf0f80e18
williamjones <willj836@gmail.com>
2022-12-14 10:41:25 +0000

    edit made

--- 02_phase_two/05_calling_apis/challenge/githubClient.js
+++ 02_phase_two/05_calling_apis/challenge/githubClient.js
@@ -4,7 +4,7 @@ class GithubClient {
   fetchRepositoryData(repoName, callback) {
     const url = `https://api.github.com/repos/${repoName}`;
     get(url, (retrievedData) => {
-      object = JSON.parse(retrievedData);
+      const object = JSON.parse(retrievedData);
       return callback(object);
     });
   }
